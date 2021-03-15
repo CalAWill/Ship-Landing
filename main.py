cb352d9e99bceb234fd135fb59f877470224aa2c
Cal <79941980+CalAWill@users.noreply.github.com>
2021-03-15 20:24:55 +0000

    Added background movement

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import pygame
 pygame.init()
 
 # Variables
-gravity = 0.03
+gravity = 0.12
 thrust = False
-thrustVal = 0.06
+thrustVal = 0.24
 
 # Make screen
 screen = pygame.display.set_mode((500, 800))
 screen.fill((0, 0, 0))
 pygame.display.set_caption("Ship Landing")
 pygame.display.set_icon(pygame.image.load("shipIcon.png"))
+
+# Background
 backLoop1 = pygame.image.load("backLoop800.png")
 backLoop2 = pygame.image.load("backLoop800.png")
 moon = pygame.image.load("moon800.png")
+backY1 = -800
+backY2 = 0
+backMove = 2
 
 # Player
 playerNThrust = pygame.image.load("shipNT128.png")
@@ -32,11 +37,12 @@ running = True
 while running:
     # Show the background for the new frame
     screen.fill((0, 0, 0))
-    screen.blit(backLoop1, (0, -800))
-    screen.blit(backLoop2, (0, 0))
+    screen.blit(backLoop1, (0, backY1))
+    screen.blit(backLoop2, (0, backY2))
     screen.blit(moon, (0, 0))
 
-    # Applies gravity to the players movement value if the ship isn't at max speed or if the ships isn't at the bottom of the screen.
+    # Applies gravity to the players movement value if the ship isn't at max speed or if the ships isn't at the
+    # bottom of the screen.
     if playerYMove != .1 or playerY < 800 - 128:
         playerYMove += gravity
 
@@ -65,10 +71,21 @@ while running:
     # Gives the player its new position
     playerY += playerYMove
 
+    # Background movement
+    # Resets the positions of the background when one drops below the screen
+    if backY1 >= 800:
+        backY1 = -800
+    if backY2 >= 800:
+        backY2 = -800
+
+    # Changes the backgrounds position for the next frame
+    backY1 += backMove
+    backY2 += backMove
+
     # Draw new positions and update screen
     if thrust:  # Draws the player with thrust image if thrust is "True"
         player(playerX, playerY, playerThrust)
-    else: # Draws the player without thrust image if thrust is "False"
+    else:  # Draws the player without thrust image if thrust is "False"
         player(playerX, playerY, playerNThrust)
 
     # Updates the screen
